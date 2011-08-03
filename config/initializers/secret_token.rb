bdf916280294add80b40d77712590a46a217456d
zff <970251106@qq.com>
2011-08-03 10:20:50 +0800

    initialize SSB project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'b156482acec4cdcbee19ccd00afe64440799e87e0dce997c59a17bd5d4101de418988bb438b4d2b7d8fbf3e8bdd494c7e4d03aafe04b93423b3b7490c69c9b66'
